6c10a96ee856c6238e3acc5e30b9b9ec965474b3
Mitchell Carroll <mitch@mitchcarroll.com>
2019-04-15 00:44:54 +0900

    edit formatting

--- src/components/sideMenu.js
+++ src/components/sideMenu.js
@@ -11,40 +11,58 @@ const SubMenu = Menu.SubMenu;
 const SideMenu = (props) => {
 
 	return (
-    // <Menu
-    //   mode="vertical"
-    //   style={{ color: 'rgb(255,255,255)', background: 'transparent'}}
-    //   onClick={e => props.onClick(e)}
-    // >
-    //   <SubMenu
-    //     key="sub1"
-    //     title={<React.Fragment><span><Icon type={props.icon} /></span><span>{props.name}</span></React.Fragment>}
-    //   >
-    //     <Menu.Item key='0' className="menuItem"><span><Icon type="youtube" /><span>Media</span></span></Menu.Item>
-    //     <Menu.Item key='1' className="menuItem">Mentions</Menu.Item>
-    //     <Menu.Item key='2' className="menuItem">Hashtags</Menu.Item>
-    //     <Menu.Item key='3' className="menuItem">Image Content</Menu.Item>
-    //     <Menu.Item key='4' className="menuItem">Tagged Locations</Menu.Item>
-    //     <Menu.Item key='5' className="menuItem">Brand Patterns</Menu.Item>
-    //     <Menu.Item key='6' className="menuItem">Tagged Accounts</Menu.Item>
-    //   </SubMenu>
-    // </Menu>
-		<div className="menu-container">
-			<Menu
-				mode="vertical"
-				style={{ color: 'rgb(255,255,255)', background: 'transparent'}}
-				onClick={e => props.onClick(e)}
-			>
-				<Menu.Item key='0' className="menuItem"><span><Icon type="youtube" /><span>Media</span></span></Menu.Item>
-				<Menu.Item key='1' className="menuItem"><span><Icon type="youtube" /><span>Mentions</span></span></Menu.Item>
-				<Menu.Item key='2' className="menuItem"><span><Icon type="youtube" /><span>Hashtags</span></span></Menu.Item>
-				<Menu.Item key='3' className="menuItem"><span><Icon type="youtube" /><span>Image Content</span></span></Menu.Item>
-				<Menu.Item key='4' className="menuItem"><span><Icon type="youtube" /><span>Tagged Locations</span></span></Menu.Item>
-				<Menu.Item key='5' className="menuItem"><span><Icon type="youtube" /><span>Brand Patterns</span></span></Menu.Item>
-				<Menu.Item key='6' className="menuItem"><span><Icon type="youtube" /><span>Tagged Accounts</span></span></Menu.Item>
-			</Menu>
-		</div>
+            
+	    <div className="menu-container">
+	      <Menu
+		mode="vertical"
+		style={{ color: 'rgb(255,255,255)', background: 'transparent'}}
+		onClick={e => props.onClick(e)}
+	      >
+		<Menu.Item key='0' className="menuItem">
+                  <span>
+                    <Icon type="youtube" /><span>Media</span>
+                  </span>
+                </Menu.Item>
 
+		<Menu.Item key='1' className="menuItem">
+                  <span>
+                    <Icon type="youtube" /><span>Mentions</span>
+                  </span>
+                </Menu.Item>
+                
+		<Menu.Item key='2' className="menuItem">
+                  <span>
+                    <Icon type="youtube" /><span>Hashtags</span>
+                  </span>
+                </Menu.Item>
+                
+		<Menu.Item key='3' className="menuItem">
+                  <span>
+                    <Icon type="youtube" /><span>Image Content</span>
+                  </span>
+                </Menu.Item>
+                
+		<Menu.Item key='4' className="menuItem">
+                  <span>
+                    <Icon type="youtube" /><span>Tagged Locations</span>
+                  </span>
+                </Menu.Item>
+
+		<Menu.Item key='5' className="menuItem">
+                  <span>
+                    <Icon type="youtube" /><span>Brand Patterns</span>
+                  </span>
+                </Menu.Item>
+                
+		<Menu.Item key='6' className="menuItem">
+                  <span>
+                    <Icon type="youtube" /><span>Tagged Accounts</span>
+                  </span>
+                </Menu.Item>
+                
+	      </Menu>
+	    </div>
+            
 	);
 }
 

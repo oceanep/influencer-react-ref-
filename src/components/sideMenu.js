6908ab29b6a50a4ff9a62528e92298cd3f2cdf11
Mitchell Carroll <mitchell.carroll@street-academy.com>
2019-04-15 17:04:10 +0900

    adding icons

--- src/components/sideMenu.js
+++ src/components/sideMenu.js
@@ -4,7 +4,10 @@ import React from 'react';
 import "./sideMenu.css";
 import "antd/dist/antd.css";
 import MediaCard from "./mediaCard.js";
-import { Card, Row, Col, Layout, Menu, Icon } from 'antd';
+import { Card, Row, Col, Layout, Menu, Icon} from 'antd';
+import { FontAwesomeIcon as FAIcon} from '@fortawesome/react-fontawesome'
+import { faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faAt,faHashtag,faImage,faHandshake } from '@fortawesome/free-solid-svg-icons'
 
 const SubMenu = Menu.SubMenu;
 
@@ -20,37 +23,37 @@ const SideMenu = (props) => {
 	      >
 		<Menu.Item key='0' className="menuItem">
                   <span>
-                    <Icon type="youtube" /><span>Media</span>
+                    <FAIcon icon={faYoutube} /><span className="menuTitle">Media</span>
                   </span>
                 </Menu.Item>
 
 		<Menu.Item key='1' className="menuItem">
                   <span>
-                    <Icon type="youtube" /><span>Mentions</span>
+                    <FAIcon icon={faAt} /><span className="menuTitle">Mentions</span>
                   </span>
                 </Menu.Item>
                 
 		<Menu.Item key='2' className="menuItem">
                   <span>
-                    <Icon type="youtube" /><span>Hashtags</span>
+                    <FAIcon icon={faHashtag} /><span className="menuTitle">Hashtags</span>
                   </span>
                 </Menu.Item>
                 
 		<Menu.Item key='3' className="menuItem">
                   <span>
-                    <Icon type="youtube" /><span>Image Content</span>
+                    <FAIcon icon={faImage} /><span className="menuTitle">Image Content</span>
                   </span>
                 </Menu.Item>
                 
 		<Menu.Item key='4' className="menuItem">
                   <span>
-                    <Icon type="youtube" /><span>Tagged Locations</span>
+                    <FAIcon className="fas fa-map-marker" /><span className="menuTitle">Tagged Locations</span>
                   </span>
                 </Menu.Item>
 
 		<Menu.Item key='5' className="menuItem">
                   <span>
-                    <Icon type="youtube" /><span>Brand Patterns</span>
+                    <FAIcon icon="faHandshake" /><span className="menuTitle">Brand Partners</span>
                   </span>
                 </Menu.Item>
                 

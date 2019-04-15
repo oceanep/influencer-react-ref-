eb9fb7eecf6f1e8581ff0ff37fd2dd94cb9a5c44
Mitchell Carroll <mitchell.carroll@street-academy.com>
2019-04-15 17:28:38 +0900

    update submenu icons

--- src/components/sideMenu.js
+++ src/components/sideMenu.js
@@ -5,9 +5,7 @@ import "./sideMenu.css";
 import "antd/dist/antd.css";
 import MediaCard from "./mediaCard.js";
 import { Card, Row, Col, Layout, Menu, Icon} from 'antd';
-import { FontAwesomeIcon as FAIcon} from '@fortawesome/react-fontawesome'
-import { faYoutube } from '@fortawesome/free-brands-svg-icons'
-import { faAt,faHashtag,faImage,faHandshake } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const SubMenu = Menu.SubMenu;
 
@@ -23,43 +21,43 @@ const SideMenu = (props) => {
 	      >
 		<Menu.Item key='0' className="menuItem">
                   <span>
-                    <FAIcon icon={faYoutube} /><span className="menuTitle">Media</span>
+                    <FontAwesomeIcon icon={['fab', 'youtube']} /><span className="menuTitle">Media</span>
                   </span>
                 </Menu.Item>
 
 		<Menu.Item key='1' className="menuItem">
                   <span>
-                    <FAIcon icon={faAt} /><span className="menuTitle">Mentions</span>
+                    <FontAwesomeIcon icon={['fas','at']} /><span className="menuTitle">Mentions</span>
                   </span>
                 </Menu.Item>
                 
 		<Menu.Item key='2' className="menuItem">
                   <span>
-                    <FAIcon icon={faHashtag} /><span className="menuTitle">Hashtags</span>
+                    <FontAwesomeIcon icon={['fas', 'hashtag']} /><span className="menuTitle">Hashtags</span>
                   </span>
                 </Menu.Item>
                 
 		<Menu.Item key='3' className="menuItem">
                   <span>
-                    <FAIcon icon={faImage} /><span className="menuTitle">Image Content</span>
+                    <FontAwesomeIcon icon={['fas', 'image']} /><span className="menuTitle">Image Content</span>
                   </span>
                 </Menu.Item>
                 
 		<Menu.Item key='4' className="menuItem">
                   <span>
-                    <FAIcon className="fas fa-map-marker" /><span className="menuTitle">Tagged Locations</span>
+                    <FontAwesomeIcon icon={['fas', 'map-marker-alt']} /><span className="menuTitle">Tagged Locations</span>
                   </span>
                 </Menu.Item>
 
 		<Menu.Item key='5' className="menuItem">
                   <span>
-                    <FAIcon icon="faHandshake" /><span className="menuTitle">Brand Partners</span>
+                    <FontAwesomeIcon icon={['fas', 'handshake']} /><span className="menuTitle">Brand Partners</span>
                   </span>
                 </Menu.Item>
                 
 		<Menu.Item key='6' className="menuItem">
                   <span>
-                    <Icon type="youtube" /><span>Tagged Accounts</span>
+                    <FontAwesomeIcon icon={['fas', 'user-circle']} /><span className="menuTitle">Tagged Accounts</span>
                   </span>
                 </Menu.Item>
                 
--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import { Provider } from 'mobx-react';
 import Main from './content';
 import Login from './components/login.js';
 import registerServiceWorker from './registerServiceWorker';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
+library.add(fab, fas);
 
 ReactDOM.render(
 	  <Main />

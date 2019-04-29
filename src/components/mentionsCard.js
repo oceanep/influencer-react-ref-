e8e7d756a76961a68781311caa66ed6aa46cd77c
Mitchell Carroll <mitchell.carroll@street-academy.com>
2019-04-29 15:51:48 +0900

    mentions is now a react component

--- src/components/mentionsCard.js
+++ src/components/mentionsCard.js
@@ -8,73 +8,78 @@ import { Card, Row, Col, Layout, Menu, Icon } from 'antd';
 const { Meta } = Card;
 const SubMenu = Menu.SubMenu;
 
-const textMentions = [
-  {
-    name: '@doyoutravelpresets',
-    amount: '4',
-    percent: '8.15'
-  },
-  {
-    name: '@lulus',
-    amount: '3',
-    percent: '4.12'
-  },
-  {
-    name: '@visittheusa',
-    amount: '2',
-    percent: '2.78'
-  }
-];
+class MentionsCard extends React.Component {
 
-const MentionsCard = (props) => {
+    constructor(props){
+        super(props);
 
-  function mapMentions(mentions) {
-
-    for (let mention of mentions) {
-      let barLength = Math.round(parseFloat(mention.percent));
-      let barStyle = {
-        width: barLength <= 10 ? `${barLength}0%` : '100%'
-      };
-      mention['barLength'] = barLength;
-      mention['barStyle'] = barStyle;
+        this.textMentions = [
+            {
+                name: '@doyoutravelpresets',
+                amount: '4',
+                percent: '8.15'
+            },
+            {
+                name: '@lulus',
+                amount: '3',
+                percent: '4.12'
+            },
+            {
+                name: '@visittheusa',
+                amount: '2',
+                percent: '2.78'
+            }
+        ];
     }
 
-    const mentionHTML = mentions.map((mention) =>
+    
+    mapMentions(mentions) {
+        
+        for (let mention of mentions) {
+            let barLength = Math.round(parseFloat(mention.percent));
+            let barStyle = {
+                width: barLength <= 10 ? `${barLength}0%` : '100%'
+          };
+            mention['barLength'] = barLength;
+            mention['barStyle'] = barStyle;
+        }
+        
+        const mentionHTML = mentions.map((mention) =>                                         
+                                         <React.Fragment
+                                           key={mention.name}
+                                         >
+                                           <Col
+                                             span={10}
+                                             className="mentions-row"
+                                           >
+                                             <u>{mention.name}</u>
+                                           </Col>
+                                           <Col
+                                             span={14}
+                                             className="mentions-row"
+                                           >
+                                             <div className="mention-bar-container">
+                                               <span
+                                                 className="mention-bar"
+                                                 style={mention.barStyle}
+                                               >
+                                                 {mention.amount}
+                                               </span>
+                                             </div><span>{mention.percent}%</span>
+                                           </Col>
+                                         </React.Fragment>
+                                        );
+        
+        return mentionHTML;
+    }
 
-      <React.Fragment
-        key={mention.name}
-      >
-        <Col
-          span={10}
-          className="mentions-row"
-        >
-          <u>{mention.name}</u>
-        </Col>
-        <Col
-          span={14}
-          className="mentions-row"
-        >
-          <div className="mention-bar-container">
-            <span
-              className="mention-bar"
-              style={mention.barStyle}
-            >
-              {mention.amount}
-            </span>
-          </div><span>{mention.percent}%</span>
-        </Col>
-      </React.Fragment>
-    );
-
-    return mentionHTML;
-  }
-
+    render(){
   	return (
-          <Row type="flex" className="mentions-container" justify="space-between" gutter={16}>
-            {mapMentions(textMentions)}
-          </Row>
+            <Row type="flex" className="mentions-container" justify="space-between" gutter={16}>
+              {this.mapMentions(this.textMentions)}
+            </Row>
   	);
-
+    }
 }
 
 export default MentionsCard;
